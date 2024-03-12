159dac5c88030515eff4d6d26212c278e4b4bfac
nithiishkumarb <nithiishkumarb.gmail.com>
2024-03-12 14:47:00 +0530

    modified routes

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ const users =require("./Routes/users")
 const dotenv=require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.json());
+app.use(express.json())
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(console.log("Connected to Mongodb")).catch(err=>console.log(err))
